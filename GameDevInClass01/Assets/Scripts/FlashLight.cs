e3c59483f5e955b1fe280fa625c815b4a108003a
AnthonyAchaeampong <117184182+AnthonyAchaeampong@users.noreply.github.com>
2023-10-31 10:53:17 -0400

    We added a

--- GameDevInClass01/Assets/Scripts/FlashLight.cs
+++ GameDevInClass01/Assets/Scripts/FlashLight.cs
@@ -10,6 +10,7 @@ public class FlashLight : MonoBehaviour
 {
 
     public Light light;
+    public AudioSource audio_source;
     
     
     
@@ -31,6 +32,13 @@ public class FlashLight : MonoBehaviour
     public void OnClick(InputAction.CallbackContext context)
     {
         light.enabled = !light.enabled;
+        //audio_source.pitch Random.Range(0.5f, 1.5f);
+        audio_source.Play();
+        
+        //float r = Random.Range(0.0f, 1.0f);
+        //float g = Random.Range(0.0f, 1.0f);
+        //float b = Random.Range(0.0f, 1.0f);
+        //light.color = new Color(r, g, b);
     }
 }
 
